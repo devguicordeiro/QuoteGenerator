fd8b6d414146c14e09d823cba7a342cfecaf41fb
Guilherme <devguicordeiro@gmail.com>
2023-04-17 20:30:56 -0300

    app working just fine. go next on

--- script.js
+++ script.js
@@ -1,26 +1,54 @@
 function generateQuote() {
   let quotes = {
-    '"The only thing we have to fear is fear itself."':
-      "- Franklin D. Roosevelt",
-    '"The only way to do great work is to love what you do."': "- Steve Jobs",
-    '"In the middle of every difficulty lies opportunity."':
-      "- Albert Einstein",
-    '"Be yourself; everyone else is already taken."': "- Oscar Wilde",
-    '"The best revenge is massive success."': "- Frank Sinatra",
-    '"Success is not final, failure is not fatal: It is the courage to continue that counts."':
-      "- Winston Churchill",
-    '"The future belongs to those who believe in the beauty of their dreams."':
-      "- Eleanor Roosevelt",
-    '"The only thing standing between you and your goal is the story you keep telling yourself as to why you can\'t achieve it."':
-      "- Jordan Belfort",
+    "-Franklin D. Roosevelt":
+      '"The only thing we have to fear is fear itself."',
+    "-Robert Frost":
+      '"In three words I can sum up everything I\'ve learned about life: it goes on."',
+    "-Socrates": '"The only true wisdom is in knowing you know nothing."',
+    "-Nelson Mandela":
+      '"The greatest glory in living lies not in never falling, but in rising every time we fall."',
+    "-Eleanor Roosevelt":
+      '"The future belongs to those who believe in the beauty of their dreams."',
+    "-Wayne Gretzky": '"You miss 100% of the shots you don\'t take."',
+    "-Confucius":
+      '"It does not matter how slowly you go as long as you do not stop."',
+    "-Walt Disney":
+      '"The way to get started is to quit talking and begin doing."',
+    "-Frank Sinatra": '"The best revenge is massive success."',
+    "-Jordan Belfort":
+      '"The only thing standing between you and your goal is the story you keep telling yourself as to why you can\'t achieve it."',
+    "-Unknown":
+      '"The harder you work for something, the greater you\'ll feel when you achieve it."',
+    "-Franklin D. Roosevelt":
+      '"The only limit to our realization of tomorrow will be our doubts of today."',
+    "-Yoda": '"The greatest teacher, failure is."',
+    "-Peter Drucker": '"The best way to predict the future is to create it."',
+    "-Nelson Mandela": '"It always seems impossible until it is done."',
+    "-Winston Churchill":
+      '"Success is walking from failure to failure with no loss of enthusiasm."',
+    "-Plato": '"The greatest wealth is to live content with little."',
+    "-Toni Morrison":
+      '"If you want to fly, you have to give up the things that weigh you down."',
+    "-Joel Brown":
+      '"The only thing that stands between you and your dream is the will to try and the belief that it is actually possible."',
+    "-Winston Churchill":
+      '"Success is not final, failure is not fatal: It is the courage to continue that counts."',
+    "-Thomas Edison":
+      "\"I have not failed. I've just found 10,000 ways that won't work.\"",
+    "-Marcus Aurelius":
+      '"The best revenge is to be unlike him who performed the injury."',
+    "-Steve Jobs": '"The only way to do great work is to love what you do."',
+    "-Zig Ziglar": '"The more you give away, the more you will have."',
+    "-Chinese Proverb":
+      '"The best time to plant a tree was 20 years ago. The second best time is now."',
   };
+  let authors = Object.keys(quotes);
 
-  let quotesString = Object.keys(quotes);
+  let author = authors[Math.floor(Math.random() * authors.length)];
 
-  let quote = quotesString[Math.floor(Math.random() * quotesString.length)];
+  let quote = quotes[author];
 
-  let author = quotes[quote];
+  document.getElementById("quote").innerHTML = quote;
 
-  document.getElementById("quote").innerHTML = quotesString;
   document.getElementById("author").innerHTML = author;
 }
